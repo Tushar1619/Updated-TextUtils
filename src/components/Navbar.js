6541ba48095f289b4d5dae5463180e16b5864a8e
Tushar Rai <tusharrai132@gmail.com>
2023-04-29 15:09:37 +0530

    final changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,23 +17,15 @@ export default function Navbar(props) {
     }
     return (
         <>
-            <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode} fixed-top`} >
-                <div className="container-fluid">
+            <nav class={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}>
+                <div class="container-fluid">
                     <Link className='navbar-brand' to="/" onClick={desc}>
                         {props.title}
                     </Link>
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        // data-bs-target="/navbarSupportedContent"
-                        aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <Link className={`nav-link ${linkItem === 'home' ? 'active' : ''}`} aria-current="page" to="/home" onClick={homeClick}>
@@ -51,18 +43,20 @@ export default function Navbar(props) {
                                 </Link>
                             </li>
                             {/* <li className="nav-item">
-                                <Link className="nav-link" to="/">
+                                 <Link className="nav-link" to="/">
                                     {props.aboutText}
-                                </Link>
+                                 </Link>
                             </li> */}
                         </ul>
-                        <button className={`btn-${props.mode === 'light' ? 'primary' : 'secondary'} btn  mx-1`} > <a href="https://644cd721ac9d3670a844a982--samurai007.netlify.app/" className="nav-link" rel="noreferrer" target="_blank" >AI Text Summarizer</a>  </button>
-                        <div className="form-check form-switch">
+                        <form class="d-flex" role="search">
+                            <button class="btn btn-primary mx-2" type="submit"> <a href="https://644cd721ac9d3670a844a982--samurai007.netlify.app/" className="nav-link" rel="noreferrer" target="_blank" >AI Text Summarizer</a></button>
+                            <div className="form-check form-switch mt-1">
 
-                            <input className="form-check-input" type="checkbox" role="switch" onClick={props.toggle} id="flexSwitchCheckDefault" />
-                            <label className={`form-check-label text-${props.mode === 'light' ? 'dark' : 'light'}`} htmlFor="flexSwitchCheckDefault">Dark mode</label>
+                                <input className="form-check-input" type="checkbox" role="switch" onClick={props.toggle} id="flexSwitchCheckDefault" />
+                                <label className={`form-check-label text-${props.mode === 'light' ? 'dark' : 'light'}`} htmlFor="flexSwitchCheckDefault">Dark mode</label>
 
-                        </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </nav>
@@ -78,4 +72,8 @@ Navbar.defaultProps = {
     title: "Title text",
     aboutText: "About"
 }
+
+
+
+
 
